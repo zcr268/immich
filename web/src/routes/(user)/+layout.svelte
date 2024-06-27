74c63c746bc2e92276e0164417c5fe81530af279
Min Idzelis <min123@gmail.com>
2024-06-26 22:32:12 -0400

    make hash work everywhere

--- web/src/routes/(user)/+layout.svelte
+++ web/src/routes/(user)/+layout.svelte
@@ -2,7 +2,8 @@
   import UploadCover from '$lib/components/shared-components/drag-and-drop-upload-overlay.svelte';
   import { page } from '$app/stores';
   import { assetViewingStore } from '$lib/stores/asset-viewing.store';
-  let { isViewing: showAssetViewer, setAsset } = assetViewingStore;
+  import { handlePromiseError } from '$lib/utils';
+  let { isViewing: showAssetViewer, setAsset, setGridScrollTarget } = assetViewingStore;
 
   // This block takes care of opening the viewer.
   // $page.data.asset is loaded by route specific +page.ts loaders if that
@@ -12,6 +13,11 @@
       setAsset($page.data.asset);
     } else {
       $showAssetViewer = false;
+    }
+  }
+  $: {
+    if ($page.url.hash) {
+      handlePromiseError(setGridScrollTarget($page.url.hash.slice(1)));
     }
   }
 </script>
